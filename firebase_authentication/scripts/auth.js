89c9a2ab6b7e09d3043aa17ce4d333a8a1a917a3
shashankpalingi <23071a6745@vnrvjiet.in>
2026-06-02 12:24:50 +0530

    added logout logic

--- firebase_authentication/scripts/auth.js
+++ firebase_authentication/scripts/auth.js
@@ -1,16 +1,16 @@
-import { auth } from './firebase.js';
-import { createUserWithEmailAndPassword }
+import { auth } from './firebase.js';    
+import { createUserWithEmailAndPassword,signOut }
 from "https://www.gstatic.com/firebasejs/12.13.0/firebase-auth.js";
 
 const signupForm = document.querySelector('#signup-form');
 
-signupForm.addEventListener('submit', (e) => {
+signupForm.addEventListener('submit',(e) => {
 
     e.preventDefault();
-
+    // get user info
     const email = signupForm['signup-email'].value;
     const password = signupForm['signup-password'].value;
-
+    // signup the user  
     createUserWithEmailAndPassword(auth, email, password)
     .then((cred) => {
         console.log(cred.user);
@@ -24,3 +24,13 @@ signupForm.addEventListener('submit', (e) => {
     });
 
 });
+
+// logout
+const logout=document.querySelector('#logout');
+logout.addEventListener('click',(e)=>{
+    e.preventDefault();
+    signOut(auth).then(()=>{
+        console.log("user signed out");
+    })
+})
+
